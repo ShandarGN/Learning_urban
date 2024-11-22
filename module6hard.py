9bd38f1e7abccb6c82ceaa033f19a49c6f28687b
ShandarGN <shandarsm@gmail.com>
2024-11-22 15:56:45 +0300

    Update module6hard.py
    Добавлены комментарии

--- module6hard.py
+++ module6hard.py
@@ -1,9 +1,11 @@
 import math
-
+# Описание класса Figure
 class Figure():
     sides_count = 0
+    # Инициализация класса
     def __init__(self, sides, red, green, blue, filled = False):
         self.__sides = []
+        # Заполнение параметров сторон в зависимости от количества и переданных данных
         if self.sides_count == 0:
             self.__sides = []
         elif len(sides) != self.sides_count:
@@ -14,10 +16,10 @@ class Figure():
                 self.__sides.append(sides[i])
         self.__color = self._Figure__is_valid_color(red, green, blue)[1]
         self.filled = filled
-
+    # Метод get_color(получение цветов)
     def get_color(self):
         return self.__color
-
+    # Метод __is_valid_color (проверка допустимости данных цветов)
     def __is_valid_color(self, red, green, blue):
         valid = [False, False, False]
         rgb = [red, green, blue]
@@ -27,28 +29,28 @@ class Figure():
             return self, None
         else:
             return self, rgb
-
+    # Метод set_color (установка цветов)    
     def set_color(self, red, green, blue):
         check = self._Figure__is_valid_color(red, green, blue)[1]
         if check:
             self.__color = self._Figure__is_valid_color(red, green, blue)[1]
-
+    # Метод __is_valid_sides (проверка допустимости данных сторон)
     def  __is_valid_sides(self, lst_sides):
         res = self.sides_count == len(lst_sides)
         for i in lst_sides:
             res = res and (i in range(0,255) and type(i) == int)
         return res
-
+    # Метод get_sides (получение данных сторон)
     def get_sides(self):
         return self._Figure__sides
-
+    # Метод set_sides (установка сторон)
     def set_sides(self, new_sides):
         if len(new_sides) == self.sides_count:
             self._Figure__sides.clear()
             self._Figure__sides = new_sides
         else:
             return None
-
+    # Медод __len (расчет периметра)
     def __len(self):
         if self.sides_count == 0:
             return None
@@ -59,25 +61,27 @@ class Figure():
             for i in self._Figure__sides:
                 perim = perim + i
             return perim
-
-
+# Описание класса Circle
 class Circle(Figure):
     sides_count = 1
+    # Метод __radius (получение радиуса круга)    
     def __radius(self):
         return self._Figure__sides[0]
-
+    # Метод get_square (расчет площади)
     def get_square(self):
         return round(math.pow(self._Figure__sides[0],2)*math.pi,3)
-
+# Описание класса Triangle
 class Triangle(Figure):
     sides_count = 3
+    # Метод get_square (расчет площади)
     def get_square(self):
         p = self._Figure__len()/2
         s = round(math.pow(p*(p-self.get_sides()[0])*(p-self.get_sides()[1])*(p-self.get_sides()[2]),0.5),3)
         return s
-
+# Описание класса Cube
 class Cube(Figure):
     sides_count = 12
+    # Переопределение параметров объекта при создании
     def __init__(self, sides, red, green, blue, filled = False):
         self._Figure__sides = []
         if len(sides) == 1:
@@ -91,7 +95,7 @@ class Cube(Figure):
                 self._Figure__sides.append(sides[i])
         self._Figure__color = self._Figure__is_valid_color(red, green, blue)[1]
         self.filled = filled
-
+    # Переопределение метода set_sides (установка сторон) из родительского класса Figure
     def set_sides(self, new_sides):
         self._Figure__sides.clear()
         if len(new_sides) == 1:
@@ -103,58 +107,9 @@ class Cube(Figure):
         else:
             for i in range(0, 12):
                 self._Figure__sides.append(new_sides[i])
-
+    # Метод get_volume (расчет объема)
     def get_volume(self):
         return math.pow(self._Figure__sides[0],3)
-
-# # Тестовые данные
-# lst_color_00 = [151, 152, 153]
-# lst_color = [101, 102, 103]
-# lst_sides = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10] # 15 чисел
-# lst_sides_00 = [11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11] # 15 чисел
-#
-# f00 = Figure(lst_sides, *lst_color_00)
-# c00 = Circle(lst_sides, *lst_color_00)
-# t00 = Triangle(lst_sides, *lst_color_00)
-# cb00 = Cube(lst_sides, *lst_color_00)
-#
-# # Блок тестирования методов
-# print('00',f00.get_sides(), c00.get_sides(), t00.get_sides()) # Получение длин сторон из Figure, Circle и Triangle
-# print('01', cb00.get_sides()) # Получение длин сторон из Cube
-# print('02',f00.get_color()) # Получение RGB из Figure
-# check_color = f00._Figure__is_valid_color(*lst_color) # Проверка переданных значений RGB на валидность
-# if check_color[1]:
-#     f00.set_color(*check_color[1]) # Установка переданных значений RGB в Figure
-# else:
-#     print(f'В переданных данных для {next(key for key, value in globals().items() if value == check_color[0])} ошибка')
-# print('03',f00.get_color()) # Получение RGB из Figure
-# print('04',f00._Figure__is_valid_sides(lst_sides)) # Проверка переданного списка сторон на тип и значение
-# print('05',f00.get_color(), c00.get_color()) # Получение RGB из Figure и Circle
-# check_color = c00._Figure__is_valid_color(*lst_color) # Проверка переданных значений RGB на валидность
-# if check_color[1]:
-#     c00.set_color(*check_color[1]) # Установка переданных значений RGB в Circle
-# else:
-#     print(f'В переданных данных для {next(key for key, value in globals().items() if value == check_color[0])} ошибка')
-# print('06',f00.get_color(), c00.get_color()) # Получение RGB из Figure и Circle
-# print('07',f00.get_sides(), c00.get_sides()) # Получение длин сторон из Figure и Circle
-# f00.set_sides(lst_sides[:1]) # Установка значений сторон Figure
-# c00.set_sides(lst_sides[:1]) # Установка значений сторон Circle
-# t00.set_sides(lst_sides[:3]) # Установка значений сторон Triangle
-# cb00.set_sides(lst_sides[:12]) # Установка значений сторон Cube списком сторон
-# print('08',f00.get_sides(), c00.get_sides(), t00.get_sides()) # Получение длин сторон из Figure, Circle и Triangle
-# print('09', cb00.get_sides()) # Получение длин сторон из Cube
-# cb00.set_sides(lst_sides_00) # Установка значений сторон Cube списком сторон при передаче числа значений больше 12
-# print('10', cb00.get_sides()) # Получение длин сторон из Cube
-# print('11',f00._Figure__len(), c00._Figure__len()) # Расчет периметра для Figure и Circle
-# print('12',t00._Figure__len(), cb00._Figure__len()) # Расчет периметра для Triangle и Cube
-# print('13',c00._Circle__radius()) # Получение радиуса для Circle
-# print('14',c00.get_square()) # Расчет площади Circle
-# print('15',t00.get_square()) # Расчет площади Triangle
-# print('16',cb00.get_volume()) # Расчет объема Cube
-# cb00.set_sides(lst_sides[:12]) # Установка значений сторон Cube, по первому значения списка сторон
-# print('17', cb00.get_sides()) # Получение длин сторон из Cube
-# print('18', cb00._Figure__len()) # Расчет периметра для Cube
-# print('19',cb00.get_volume()) # Расчет объема Cube
 print('-'*20)
 # Отработка примеров
     # Исходные данные
