b5bba2253fd284bdea0203c61e8e247bb9990c07
ShandarGN <shandarsm@gmail.com>
2025-01-20 19:25:21 +0300

    Add files via upload
    Домашнее задание по теме "Хендлеры обработки сообщений"

---
+++ Module_13_0_2.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ''
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage = MemoryStorage())
+
+command_dict = {'start': 'Привет! Я бот помогающий твоему здоровью', 'end': 'До свидания'}
+
+@dp.message_handler(commands=['start', 'end'])
+async def start_mes(mess):
+    print(command_dict[mess['text'][1:]])
+
+@dp.message_handler()
+async def all_mess(mess):
+    print('Введите команду /start, чтобы начать общение и /end, чтобы завершить')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates = True )
